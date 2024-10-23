4eb062e60c8ca7439ecdd5986b20afda94e2525f
Ihar <iusmanof@gmail.com>
2024-10-24 06:03:09 +1400

    fix: thisAmount in comedy and test fixed

--- statement.test.js
+++ statement.test.js
@@ -4,9 +4,9 @@ test('correctly generates statement for BigCo', () => {
   const expectedOutput =
     `Statement for BigCo\n` +
     `   Hamlet: $290.00   (55 seats)\n` +
-    `   As You Like It: $435.00   (45 seats)\n` +
+    `   As You Like It: $570.00   (45 seats)\n` +
     `   Othello: $140.00   (40 seats)\n` +
-    `Amount owed is $865.00\n` +
+    `Amount owed is $1,000.00\n` +
     `You earned 59 credits\n`;
 
   expect(statement(invoices, plays)).toBe(expectedOutput);
