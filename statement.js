72d9a3a0f4f5015451cde07c94eb99e393472211
Ihar <iusmanof@gmail.com>
2024-10-29 05:37:17 +1400

    refactor: function extraction

--- statement.js
+++ statement.js
@@ -33,6 +33,16 @@ function volumeCreditsFor(aPerfomance) {
   return result;
 }
 
+function totalVolumeCredits(invoice) {
+  let volumeCredits = 0;
+
+  for (let perf of invoice[0].performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+
+  return volumeCredits;
+}
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let result = `Statement for ${invoice[0].customer}\n`;
@@ -43,10 +53,10 @@ function statement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = 0;
-  for (let perf of invoice[0].performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
+  let volumeCredits = totalVolumeCredits(invoice);
+  // for (let perf of invoice[0].performances) {
+  //   volumeCredits += volumeCreditsFor(perf);
+  // }
 
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
