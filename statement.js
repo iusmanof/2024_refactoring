8f464e3d20180cc358f4300ee88a3df8d0927c8a
Ihar <iusmanof@gmail.com>
2024-10-30 05:53:39 +1400

    refact: reduce in func

--- statement.js
+++ statement.js
@@ -55,23 +55,11 @@ function renderPlainText(data, invoice, plays) {
 }
 
 function totalAmount(data) {
-  let result = 0;
-
-  for (let perf of data.performances) {
-    result += perf.amount;
-  }
-
-  return result;
+  return data.performances.reduce((total, p) => total + p.amount, 0)
 }
 
 function totalVolumeCredits(data) {
-  let result = 0;
-
-  for (let perf of data.performances) {
-    result += perf.volumeCredits;
-  }
-
-  return result;
+  return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
 }
 
 function usd(aNumber) {
