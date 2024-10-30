da470704a6e92a54812124186066520eb455377a
Ihar <iusmanof@gmail.com>
2024-10-31 05:53:50 +1400

    fix: run lint and format

--- src/statement.js
+++ src/statement.js
@@ -32,19 +32,17 @@ export function statement(invoice, plays) {
 
 function renderHtml(data) {
   let result = `<h1>Statement for ${data.customer}</h1>\n`;
-  result += `<table>\n`
-  result += `<tr><th>play</th><th>seats</th><th>cost</th></tr>\n`
+  result += '<table>\n';
+  result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>\n';
   for (let perf of data.performances) {
-    result += `<tr><td>${perf.play.name}</td>`
-    result += `<td>${usd(perf.amount)}</td>`
-    result += `<td>${perf.audience}</td></tr>\n`
-    // result += `   ${perf.play.name}: ${usd(perf.amount)}`;
-    // result += `   (${perf.audience} seats)\n`;
+    result += `<tr><td>${perf.play.name}</td>`;
+    result += `<td>${usd(perf.amount)}</td>`;
+    result += `<td>${perf.audience}</td></tr>\n`;
   }
 
   result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
   result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
-  result += `</table>\n`
+  result += '</table>\n';
   return result;
 }
 
--- src/statement.test.js
+++ src/statement.test.js
@@ -4,7 +4,7 @@ test('correctly generates statement for BigCo', () => {
   const expectedOutput =
     `<h1>Statement for BigCo</h1>\n` +
     `<table>\n` +
-    `<tr><th>play</th><th>seats</th><th>cost</th></tr>\n`+
+    `<tr><th>play</th><th>seats</th><th>cost</th></tr>\n` +
     `<tr><td>Hamlet</td><td>$290.00</td><td>55</td></tr>\n` +
     `<tr><td>As You Like It</td><td>$570.00</td><td>45</td></tr>\n` +
     `<tr><td>Othello</td><td>$140.00</td><td>40</td></tr>\n` +
