6cdd35a3b5499d29689abb360d43cd9f1ba9707b
Ihar <iusmanof@gmail.com>
2024-10-31 05:52:54 +1400

    refact: rename func and add html tags

--- src/statement.js
+++ src/statement.js
@@ -27,19 +27,24 @@ export const plays = {
 };
 
 export function statement(invoice, plays) {
-  return renderPlainText(createStatementData(invoice, plays));
+  return renderHtml(createStatementData(invoice, plays));
 }
 
-function renderPlainText(data, invoice, plays) {
-  let result = `Statement for ${data.customer}\n`;
-
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += `<table>\n`
+  result += `<tr><th>play</th><th>seats</th><th>cost</th></tr>\n`
   for (let perf of data.performances) {
-    result += `   ${perf.play.name}: ${usd(perf.amount)}`;
-    result += `   (${perf.audience} seats)\n`;
+    result += `<tr><td>${perf.play.name}</td>`
+    result += `<td>${usd(perf.amount)}</td>`
+    result += `<td>${perf.audience}</td></tr>\n`
+    // result += `   ${perf.play.name}: ${usd(perf.amount)}`;
+    // result += `   (${perf.audience} seats)\n`;
   }
 
-  result += `Amount owed is ${usd(data.totalAmount)}\n`;
-  result += `You earned ${data.totalVolumeCredits} credits\n`;
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  result += `</table>\n`
   return result;
 }
 
--- src/statement.test.js
+++ src/statement.test.js
@@ -2,12 +2,15 @@ import { statement, invoices, plays } from './statement'; // no .js
 
 test('correctly generates statement for BigCo', () => {
   const expectedOutput =
-    `Statement for BigCo\n` +
-    `   Hamlet: $290.00   (55 seats)\n` +
-    `   As You Like It: $570.00   (45 seats)\n` +
-    `   Othello: $140.00   (40 seats)\n` +
-    `Amount owed is $1,000.00\n` +
-    `You earned 59 credits\n`;
+    `<h1>Statement for BigCo</h1>\n` +
+    `<table>\n` +
+    `<tr><th>play</th><th>seats</th><th>cost</th></tr>\n`+
+    `<tr><td>Hamlet</td><td>$290.00</td><td>55</td></tr>\n` +
+    `<tr><td>As You Like It</td><td>$570.00</td><td>45</td></tr>\n` +
+    `<tr><td>Othello</td><td>$140.00</td><td>40</td></tr>\n` +
+    `<p>Amount owed is <em>$1,000.00</em></p>\n` +
+    `<p>You earned <em>59</em> credits</p>\n` +
+    `</table>\n`;
 
   expect(statement(invoices, plays)).toBe(expectedOutput);
 });
