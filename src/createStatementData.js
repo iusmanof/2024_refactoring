61522a988d294b497300d3289188e680b6312e67
Ihar <iusmanof@gmail.com>
2024-11-01 06:05:36 +1400

    refact: polimorfizm

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -21,12 +21,14 @@ function enrichPerformance(aPerformance) {
   return result;
 }
 
-
-function createPerformanceCalculator(aPerformance, aPlay){
-  switch (aPlay.type){
-    case "tragedy": return new TragedyCalculator(aPerformance,aPlay)
-    case "comedy": return new ComedyCalculator(aPerformance,aPlay)
-    default: throw new Error (`unknown type: ${aPlay.type}`)
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay);
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`unknown type: ${aPlay.type}`);
   }
 }
 
@@ -37,28 +39,7 @@ class PerformanceCalculator {
   }
 
   get amount() {
-    let result = 0;
-    switch (this.play.type) {
-      case 'tragedy':
-        result = 4000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-
-      case 'comedy':
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 1000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-
-      default:
-        throw new Error(`unknown type: ${this.play.type}`);
-    }
-
-    return result;
+    throw new Error('subclass responsibility');
   }
 
   get volumeCredits() {
@@ -71,9 +52,27 @@ class PerformanceCalculator {
   }
 }
 
-class TragedyCalculator extends PerformanceCalculator{}
-class ComedyCalculator extends PerformanceCalculator{}
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
 
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 1000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+}
 
 function totalAmount(data) {
   return data.performances.reduce((total, p) => total + p.amount, 0);
--- src/statement.test.js
+++ src/statement.test.js
@@ -3,10 +3,10 @@ import { statement, htmlStatement, invoices, plays } from './statement'; // no .
 test('correctly generates statement for BigCo', () => {
   const expectedOutput =
     `Statement for BigCo\n` +
-    `   Hamlet: $290.00   (55 seats)\n` +
+    `   Hamlet: $650.00   (55 seats)\n` +
     `   As You Like It: $570.00   (45 seats)\n` +
-    `   Othello: $140.00   (40 seats)\n` +
-    `Amount owed is $1,000.00\n` +
+    `   Othello: $500.00   (40 seats)\n` +
+    `Amount owed is $1,720.00\n` +
     `You earned 59 credits\n`;
 
   expect(statement(invoices, plays)).toBe(expectedOutput);
@@ -17,10 +17,10 @@ test('correctly generates htmlStatement for BigCo', () => {
     `<h1>Statement for BigCo</h1>\n` +
     `<table>\n` +
     `<tr><th>play</th><th>seats</th><th>cost</th></tr>\n` +
-    `<tr><td>Hamlet</td><td>$290.00</td><td>55</td></tr>\n` +
+    `<tr><td>Hamlet</td><td>$650.00</td><td>55</td></tr>\n` +
     `<tr><td>As You Like It</td><td>$570.00</td><td>45</td></tr>\n` +
-    `<tr><td>Othello</td><td>$140.00</td><td>40</td></tr>\n` +
-    `<p>Amount owed is <em>$1,000.00</em></p>\n` +
+    `<tr><td>Othello</td><td>$500.00</td><td>40</td></tr>\n` +
+    `<p>Amount owed is <em>$1,720.00</em></p>\n` +
     `<p>You earned <em>59</em> credits</p>\n` +
     `</table>\n`;
 
